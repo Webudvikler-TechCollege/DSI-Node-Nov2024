fd75e1abc7b2aae4582ea18cbdba02e2cf8aa0c9
Webudvikler Hovedforløb <heka@techcollege.dk>
2024-11-26 10:13:37 +0100

    Added Cors settings

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { songController } from './Controllers/songController.js';
 import { artistController } from './Controllers/artistController.js';
 dotenv.config();
 const port = process.env.PORT
 const app = express();
-
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
+// App Settings som sikrer CORS adgang via browser
+app.use((req, res, next) => {
+	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.append('Access-Control-Allow-Credentials', true);
+	res.append('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+})
 
 // Route til forside
 app.get('/', (req,res) => {
